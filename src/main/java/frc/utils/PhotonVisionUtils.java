f231ec6e2731c970f496c600efceb00ff0d1758a
IamIisI <titusj915@gmail.com>
2024-11-13 17:07:03 -0800

    Add readCameraData to PhotonVisionUtils

--- src/main/java/frc/utils/PhotonVisionUtils.java
+++ src/main/java/frc/utils/PhotonVisionUtils.java
@@ -1,6 +1,17 @@
 package frc.utils;
 
+import java.util.List;
+
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.targeting.TargetCorner;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class PhotonVisionUtils {
     PhotonCamera arducam_1;
@@ -14,6 +25,39 @@ public class PhotonVisionUtils {
         arducam_2 = new PhotonCamera("Arducam_2");
         arducam_3 = new PhotonCamera("Arducam_3");
         arducam_4 = new PhotonCamera("Arducam_4");
+
+    }
+
+    public void readCameraData(PhotonCamera camera) {
+        
+        List<PhotonPipelineResult> results = camera.getAllUnreadResults();
+
+        PhotonPipelineResult result = results.get(0);
+
+        boolean hasTargets = result.hasTargets();
+
+        if(hasTargets) {
+            PhotonTrackedTarget target = result.getBestTarget();
+
+            // Get information from target.
+            double yaw = target.getYaw();
+            double pitch = target.getPitch();
+            double area = target.getArea();
+            double skew = target.getSkew();
+            Transform3d pose = target.getBestCameraToTarget();
+            List<TargetCorner> corners = target.getDetectedCorners();
+
+            SmartDashboard.putNumber("Target Yaw", yaw);
+            SmartDashboard.putNumber("Target Pitch", pitch);
+            SmartDashboard.putNumber("Target Area", area);
+            SmartDashboard.putNumber("Target Skew", skew);
+
+            SmartDashboard.putNumber("Target PoseX", pose.getX());
+            SmartDashboard.putNumber("Target PoseY", pose.getY());
+            SmartDashboard.putNumber("Target PoseZ", pose.getZ());
+
+        }
+
         
     }
 }
