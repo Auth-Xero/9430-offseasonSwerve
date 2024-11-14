9f12b26133891882e119bdbe722124b0caea9ca9
IamIisI <titusj915@gmail.com>
2024-11-13 17:53:20 -0800

    Add logging of A button to SmartDashboard

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,6 +21,7 @@ import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -43,6 +44,10 @@ public class RobotContainer {
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
+
+    SmartDashboard.putString("A Button", "" + m_driverController.getAButton());
+
+
     // Configure the button bindings
     configureButtonBindings();
 
@@ -57,8 +62,7 @@ public class RobotContainer {
                 -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
                 true, true),
             m_robotDrive));
-    
-    SmartDashboard.putString("Test", "tested");
+
   }
 
   /**
@@ -75,6 +79,9 @@ public class RobotContainer {
         .whileTrue(new RunCommand(
             () -> m_robotDrive.setX(),
             m_robotDrive));
+    new JoystickButton(m_driverController, XboxController.Button.kA.value)
+        .onTrue(new InstantCommand(() -> SmartDashboard.putString("A Button", "" + m_driverController.getAButton())))
+        .onFalse(new InstantCommand(() -> SmartDashboard.putString("A Button", "" + m_driverController.getAButton())));
   }
 
   /**
