c03d7c2552e1b975a661b6c504f1d98cc5b5f2e8
IamIisI <titusj915@gmail.com>
2024-12-03 19:36:16 -0800

    Revert "Merge branch 'swerve-tweaks' into elastic-dashboard"
    This reverts commit 4bc263ad267cdbe057e9003b00bcc28eb87f2056, reversing
    changes made to 9476031e85e8dc88e9e836fbf782b76020253145.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -64,7 +64,7 @@ public final class Constants {
     public static final int kFrontRightTurningCanId = 14;
     public static final int kRearRightTurningCanId = 16;
 
-    public static final boolean kGyroReversed = true;
+    public static final boolean kGyroReversed = false;
   }
 
   public static final class ModuleConstants {
@@ -121,7 +121,7 @@ public final class Constants {
 
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
-    public static final double kDriveDeadband = 0.08;
+    public static final double kDriveDeadband = 0.05;
   }
 
   public static final class AutoConstants {
